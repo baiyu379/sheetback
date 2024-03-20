fef600d8c64ee854b14c1f82542b9e969f23f366
baiyu379 <157329385+baiyu379@users.noreply.github.com>
2024-03-20 10:24:58 +0900

    Update heat_mat_streamlit_back.py

--- heat_mat_streamlit_back.py
+++ heat_mat_streamlit_back.py
@@ -48,10 +48,9 @@ data6 = np.exp(-((X - center_x6)**2 + (Y - center_y6)**2) / (2 * sigma6**2))
 
 # データ合算の係数をサイドバーに追加
 coeff_data1 = st.sidebar.slider('Coefficient for Data 1', min_value=0.0, max_value=10.0, value=1.0)
-coeff_data2 = st.sidebar.slider('Coefficient for Data 2', min_value=0.0, max_value=10.0, value=1.0)
 
 # 各データを合算
-data = data1 + data2 + data3 + data4 + coeff_data1 *data5 + coeff_data2 * data6
+data = data1 + data2 + data3 + data4 + coeff_data1 *data5 + coeff_data1 * data6
 
 # データを平滑化
 data_smoothed = gaussian_filter(data, sigma=14)
